6dcf0617c3d7dfdad550eff9eed79b1e8914406e
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-27 21:00:43 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin'),
+path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MonacoWebpackPlugin()
+  ]
+};
