bfbc814a8927484658c8482d24d0b53bd38f3247
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-27 21:01:28 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { Doc } from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding } from 'y-monaco';
+import { editor } from 'monaco-editor';
+
+const ydoc = new Doc();
+const provider = new WebrtcProvider(window.location.pathname, ydoc);
+
+const mEditor = editor.create(document.getElementById('monaco-editor'), {
+  language: 'java',
+  theme: 'vs-dark',
+  automaticLayout: true,
+  wordWrap: 'on',
+  minimap: {
+    enabled: false
+  }
+});
+
+const monacoBinding = new MonacoBinding(
+  ydoc.getText(),
+  mEditor.getModel(),
+  new Set([mEditor]),
+  provider.awareness
+);
